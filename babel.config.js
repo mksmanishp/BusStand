f87f3adc3e600c0dc817477eb8c99deca14840ea
manish.kumar <kumar.manish@medikabazaar.com>
2025-03-31 18:40:21 +0530

    app.tsx

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,10 @@
-module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+const plugin = require('tailwindcss');
+const {preset} = require('./jest.config');
+
+module.exports = function (app) {
+  app.cache(true);
+  return {
+    presets: ['module:@react-native/babel-presets', 'nativewind/babel'],
+    plugin: ['react-native-reanimated/plugin'],
+  };
 };
